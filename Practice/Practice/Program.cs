a1ae6c57e98a6241c278854a8d7c78efb3977734
Zach <zach_hirst@live.com>
2018-10-31 12:56:41 -0500

    Push

---
+++ Practice/Practice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String name = "Zach Hirst";
+            String YourAge;
+            String YourName;
+            int age = 17;
+            int Addage = 11;
+
+            Console.WriteLine("Hello, my name is {0} and I am {1} years old. " , name, age);
+            Console.WriteLine("What's your name?");
+            YourName = Console.ReadLine();
+            Console.WriteLine("And your age?");
+            YourAge = Console.ReadLine();
+            Console.WriteLine("Gosh, you're only {0}!!?? Well, {1} it's nice to meet you.", YourAge, YourName);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TacoTruck/TacoTruck/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TacoTruck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("          ____________________________                   ");
+            Console.WriteLine("          |                           L                  ");
+            Console.WriteLine("          |  TTTTT    AA      CC   OO  L                 ");
+            Console.WriteLine("          |    T     A _A   C     O  O  L                ");
+            Console.WriteLine("     _____|____T____A____A____CC___OO____L____           ");
+            Console.WriteLine("     |                                     |             ");
+            Console.WriteLine("     |              ________________        L            ");
+            Console.WriteLine("     |              |              |         L           ");
+            Console.WriteLine("     |              |       O      |          L          ");
+            Console.WriteLine("     |              |______(|)_____|          --------   ");
+            Console.WriteLine("     |       _ - _                     _ - _         |   ");
+            Console.WriteLine("     |     (  000  )                 (  000  )       |   ");
+            Console.WriteLine("     ------  00000   ----------------  00000   -------   ");
+            Console.WriteLine("              000                       000              ");
+            Console.WriteLine("                                                         ");
+            Console.ReadLine();
+        }
+    }
+}
