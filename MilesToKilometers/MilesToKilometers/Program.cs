ec692006c746cdb180d6e0c8097e3be775b87a49
Zach <zach_hirst@live.com>
2018-11-02 12:32:06 -0500

    Push

---
+++ MilesToKilometers/MilesToKilometers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilesToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double KiloPerMile = 1.6;
+            double Distance;
+            double Conversion;
+
+            Console.WriteLine("What is the distance in mile that you would like to convert?");
+            Distance = double.Parse(Console.ReadLine());
+            Conversion = Distance * KiloPerMile;
+            Console.WriteLine("The Distance in miles is {0} , and the distance in kilometers is {1} ", Distance, Conversion);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProjectedRaises/ProjectedRaises/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectedRaises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            double YearlyRaise = 1.04;
+
+            String EmployeeChoice;
+
+            Console.WriteLine("Please Select and employee.");
+            Console.WriteLine("Kane");
+            Console.WriteLine("Crhis");
+            Console.WriteLine("Zach");
+            Console.WriteLine("Sal");
+           
+            EmployeeChoice = Console.ReadLine();
+          
+            if(EmployeeChoice =="Kane")
+            {
+                double KaneSalary;
+                
+                Console.WriteLine("Please Enter His salary");
+                KaneSalary = double.Parse(Console.ReadLine());
+                double Kconversion = (KaneSalary*YearlyRaise); 
+                Console.WriteLine("Kane's projected growth of salary for the next year is {0}" , Kconversion);
+                Console.ReadLine();
+            }
+            else if(EmployeeChoice == "Chris")
+            {
+                double ChrisSalary;
+
+                Console.WriteLine("Please Enter His salary");
+                ChrisSalary = double.Parse(Console.ReadLine());
+                double Cconversion = (ChrisSalary * YearlyRaise);
+                Console.WriteLine("Chris' projected growth of salary for the next year is {0}", Cconversion);
+                Console.ReadLine();
+            }
+            else if(EmployeeChoice == "Zach")
+            {
+                double ZachSalary;
+
+                Console.WriteLine("Please Enter His salary");
+                ZachSalary = double.Parse(Console.ReadLine());
+                double Zconversion = (ZachSalary * YearlyRaise);
+                Console.WriteLine("Zach's projected growth of salary for the next year is {0}", Zconversion);
+                Console.ReadLine();
+            }
+            else if (EmployeeChoice == "Sal")
+            {
+                double SalSalary;
+
+                Console.WriteLine("Please Enter His salary");
+                SalSalary = double.Parse(Console.ReadLine());
+                double Sconversion = (SalSalary * YearlyRaise);
+                Console.WriteLine("Sal's projected growth of salary for the next year is {0}", Sconversion);
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("User not available, sorry. ");
+                Console.ReadLine(); 
+            }
+        }
+    }
+}
