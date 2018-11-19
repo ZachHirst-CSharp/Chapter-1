c3b2271ea7ce6f6c75362aa1b763fa0a1c7f8c70
Zach <zach_hirst@live.com>
2018-11-19 12:21:29 -0600

    push

--- MadLib/MadLib/Program.cs
+++ MadLib/MadLib/Program.cs
@@ -27,7 +27,7 @@ namespace MadLib
             color = Console.ReadLine();
             Console.WriteLine("Please enter a word of your choice ending in 'est'.");
             est = Console.ReadLine();
-            Console.WriteLine("Please enter a pluar body part. ");
+            Console.WriteLine("Please enter a plural body part. ");
             bodypartplural = Console.ReadLine();
             Console.WriteLine("Please enter an animal of your choice. ");
             animal = Console.ReadLine();
@@ -42,7 +42,11 @@ namespace MadLib
               
             c = b - a;
 
-            Console.WriteLine(" The {0} {1} ");
+            Console.WriteLine(" The {0} {1} was eating {2} {3} when you found it. It was the {4} {5}", color, animal, a, bodypartplural, est, animal);
+            Console.WriteLine(" you have ever seen. {0} was the {1}'s next victim... ", noun, animal);
+            Console.WriteLine("However only {0} miles from the {1} there are {2} just ", b, animal, pluralnoun);
+            Console.WriteLine("waiting to die..");
+            Console.ReadLine();
         }
     }
 }
